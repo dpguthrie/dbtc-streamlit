f0b1f589edcb80eb4aaa0f119ed0796833285220
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-03-31 16:00:14 -0600

    Include environment level analysis

--- pages/03_📈_Analysis.py
+++ pages/03_📈_Analysis.py
@@ -7,7 +7,14 @@ import altair as alt
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+from st_aggrid import AgGrid
+from st_aggrid import JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 from streamlit_plotly_events import plotly_events
+
+# first party
+from utils import agstyler
+from utils.helpers import local_css
 
 
 st.set_page_config(
@@ -74,17 +81,17 @@ def get_minutes(time_str: str):
     return round(int(h) * 60 + int(m) + (int(s) / 60), 1)
 
 
-def get_all_runs():
+def get_all_runs(**kwargs):
     all_data = []
     for OFFSET in range(0, st.session_state.n_runs, 100):
         data = client.dynamic_request(
             st.session_state.dbtc_client.cloud,
             'list_runs',
             st.session_state.account_id,
-            job_definition_id=st.session_state.job_id,
             offset=OFFSET,
             limit=100,
             order_by='-id',
+            **kwargs,
         ).get('data', [])
         if len(data) == 0:
             break
@@ -93,6 +100,53 @@ def get_all_runs():
     df = pd.DataFrame(all_data)
     return _enhance_df(df)
 
+
+
+
+def get_env_runs_stats(df: pd.DataFrame):
+    df['job_name'] = df['job'].apply(lambda x: x.get('name'))
+    df['is_success'] = df['status_humanized'] == 'Success'
+    df = df.groupby('job_name').agg(
+        avg_runtime=('run_duration_m', 'mean'),
+        total_runs=('id', 'count'),
+        total_success=('is_success', 'sum'),
+        last_run=('started_at', 'max'),
+        last_runtime=('run_duration_m', 'first'),
+        std_runtime=('run_duration_m', 'std'),
+        max_runtime=('run_duration_m', 'max'),
+        account_id=('account_id', 'mean'),
+        project_id=('project_id', 'mean'),
+        job_id=('job_definition_id', 'mean'),
+    )
+    df['success_rate'] = round(df['total_success'] / df['total_runs'] * 100, 2)
+    df['url'] = df.apply(lambda x: f'https://cloud.getdbt.com/deploy/{int(x.account_id)}/projects/{int(x.project_id)}/jobs/{int(x.job_id)}', axis=1)
+    cols = ['avg_runtime', 'last_runtime', 'std_runtime', 'max_runtime']
+    df[cols] = df[cols].round(1)
+    return df[[
+        'total_runs',
+        'total_success',
+        'success_rate',
+        'last_run',
+        'last_runtime',
+        'avg_runtime',
+        'max_runtime',
+        'std_runtime',
+        'url',
+        'job_id',
+    ]]
+    
+    
+def get_aggregate_stats(df: pd.DataFrame):
+    total_runs = df['total_runs'].sum()
+    total_success = df['total_success'].sum()
+    max_runtime = df['max_runtime'].max()
+    success_rate = total_success / total_runs
+    return {
+        'success_rate': success_rate,
+        'completed_runs': total_success,
+        'max_runtime': max_runtime,
+    }
+    
 
 def build_last_n_runs_chart(runs_df: pd.DataFrame):
 
@@ -118,7 +172,6 @@ def build_last_n_runs_chart(runs_df: pd.DataFrame):
         xaxis_title='Date',
         yaxis_title='Time (minutes)',
         hovermode='closest',
-        
     ).update_traces(
         marker=dict(
             color='white',
@@ -126,7 +179,6 @@ def build_last_n_runs_chart(runs_df: pd.DataFrame):
                 color=list(map(_set_color, runs_df['status_humanized'])),
                 width=3,
             ),
-            size=10,
             
         ),       
     )
@@ -234,6 +286,34 @@ def build_all_model_timing_chart(df, top_n: int = 5):
     return fig
 
 
+def get_intersections():
+    def model_in_job(job_name, model_name):
+        return model_name in job_models[job_name]
+    
+    job_models = {}
+    for job_id, job_definition in st.session_state.jobs.items():
+        is_ci_job = any([job_definition['triggers'][x] for x in ['github_webhook', 'git_provider_webhook']])
+        if not False:
+            data = client.dynamic_request(
+                st.session_state.dbtc_client.metadata,
+                'get_models',
+                job_id,
+                fields=['runGeneratedAt', 'name']
+            )['data']
+            if data is not None:
+                job_models[job_definition['name']] = [
+                    m['name'] for m in data['models'] if m['runGeneratedAt'] is not None
+                ]
+                
+    df = pd.DataFrame(columns=job_models.keys(), index=list(set(
+        [m for job in job_models.values() for m in job]
+    )))
+    df = df.apply(lambda x: pd.DataFrame(x).apply(lambda y: model_in_job(x.name, y.name), axis=1))*1
+    df['total_jobs'] = df.iloc[:].sum(axis=1)
+    df = df.loc[df['total_jobs'] > 1].sort_index().iloc[:, :-1].T
+    return df
+
+
 def get_model_timing_data(unique_id: str, run_ids: List[int]):
     data = []
     progress_bar = st.progress(0)
@@ -304,7 +384,7 @@ st.markdown('''
 # Create widgets
 inputs.get_account_widget()
 inputs.get_project_widget()
-inputs.get_job_widget()
+inputs.get_environment_widget(type='deployment')
 runs = st.number_input(
     label='Number of Runs',
     min_value=100,
@@ -313,12 +393,51 @@ runs = st.number_input(
     key='n_runs'
 )
 
-if len(st.session_state.jobs.keys()) > 0:
+if len(st.session_state.environments.keys()) > 0:
     
     selected_model: List[Dict] = None
     
-    # All runs for specific job
-    runs_df = get_all_runs()
+    # All runs for specific environment
+    env_runs_df = get_all_runs(
+        environment_id=st.session_state.environment_id,
+        include_related=['job'],
+    )
+    
+    env_runs_df_xf = get_env_runs_stats(env_runs_df).reset_index()
+    
+    # Aggregating stats from xf df
+    agg_stats = get_aggregate_stats(env_runs_df_xf)
+    
+    col1, col2, col3 = st.columns(3)
+    col1.metric('Success Rate', f'{agg_stats["success_rate"] * 100}%')
+    col2.metric('Completed Runs', agg_stats['completed_runs'])
+    col3.metric('Longest Runtime', agg_stats['max_runtime'])
+    
+    xf_formatter = {
+        'job_name': ('Job', {**agstyler.PINLEFT, 'width': 250, 'cellRenderer': JsCode('''
+                function(params) {console.log(params.data.url); return '<a href="' + params.data.url + '" target="_blank">' + params.value + '</a>'}
+            ''')}),
+        'total_runs': ('Runs', {'width': 75}),
+        'success_rate': ('Success %', {'width': 100}),
+        'last_run': ('Last Run', {}),
+        'last_runtime': ('Last Runtime', {'width': 75}),
+        'avg_runtime': ('Avg', {'width': 75}),
+        'max_runtime': ('Max', {'width': 75}),
+        'std_runtime': ('Std', {'width': 75}),
+    }
+
+    xf_grid = agstyler.draw_grid(
+        env_runs_df_xf,
+        formatter=xf_formatter,
+        selection=None,
+    )
+
+    inputs.get_job_widget(environment_id=st.session_state.environment_id)
+    
+    runs_df = get_all_runs(
+        job_definition_id=st.session_state.job_id,
+        include_related=['job']
+    )
     
     # Create line chart for last N runs
     runs_fig = build_last_n_runs_chart(runs_df)
@@ -369,3 +488,6 @@ if len(st.session_state.jobs.keys()) > 0:
                 get_sql(model_df, model_df.loc[model_run_index]['runId'])
             else:
                 get_sql(model_df, runs_df.loc[run_index]['id'])
+
+
+local_css('style.css')
